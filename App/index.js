c803a6246c3ef0ce92f790d36cb80b0f7985d35c
bhzd <bhzd.bakhshayesh@gmail.com>
2023-06-16 14:08:54 +0330

    code

--- App/index.js
+++ App/index.js
@@ -1,10 +1,8 @@
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './router/StackNavigator';
 
 
 const App = () => {
-
   return (
     <NavigationContainer>
       <StackNavigator/>
