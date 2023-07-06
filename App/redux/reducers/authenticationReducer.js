ca0019263be61c0e0379a39fe54900887f8c0842
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-06 17:10:18 +0330

    code

--- App/redux/reducers/authenticationReducer.js
+++ App/redux/reducers/authenticationReducer.js
@@ -1,9 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const authenticationReducer = (state = false, action) => {
   switch (action.type) {
     case 'IS_LOGIN':
       return !!action.payload;
     case 'LOGOUT':
-      return false;
+      async () => {
+        await AsyncStorage.removeItem('token');
+        await AsyncStorage.clear();
+        return false;
+      };
+      break;
     default:
       return state;
   }
