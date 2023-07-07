c99292672030d3facaadf299f369c98eced4a17d
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-07 15:01:09 +0330

    code

--- App/router/StackNavigator/index.js
+++ App/router/StackNavigator/index.js
@@ -5,7 +5,7 @@ import SignInScreen from '@app/screens/SignInScreen';
 import Authorization from '@app/screens/Authorization';
 import { useSelector, useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { loginAction } from '@app/redux/actions/authenticationActions';
+import { logOutAction, loginAction } from '@app/redux/actions/authenticationActions';
 import { loading } from '@app/redux/actions/loadingActions';
 import SplashScreen from '@app/screens/SplashScreen';
 import DirectScreen from '@app/screens/DirectScreen';
@@ -20,6 +20,8 @@ const StackNavigator = () => {
       try {
         const token = await AsyncStorage.getItem('token');
         dispatch(loginAction(token));
+      } catch (e) {
+        dispatch(logOutAction());
       } finally {
         dispatch(loading(false));
       }
