da47a4f38c2efce53864d3987d7d466062ab2bac
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-07 14:32:08 +0330

    code

--- App/screens/SplashScreen/index.js
+++ App/screens/SplashScreen/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+// eslint-disable-next-line no-undef
+const source = require('@app/assets/images/splash.png');
 
 const SplashScreen = () => {
   return (
     <View style={styles.splash}>
-      <Text>SplashScreen</Text>
+      <Image style={styles.icon} source={source} />
+      <Text>Instagram</Text>
     </View>
   );
 };
@@ -13,4 +17,5 @@ export default SplashScreen;
 
 const styles = StyleSheet.create({
   splash: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  icon: { width: 100, height: 100 },
 });
