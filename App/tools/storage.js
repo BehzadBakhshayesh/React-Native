7d38e2a387f2113e12c6d5578034041f176f30d8
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-09 23:22:30 +0330

    code

--- App/tools/storage.js
+++ App/tools/storage.js
@@ -10,15 +10,15 @@ export const setStorage = ({ key, value }) => {
   AsyncStorage.setItem(key, val);
 };
 
-export const getStorage = async ({ key, isJson }) => {
-  try {
-    const value = await AsyncStorage.getItem(key);
+// export const getStorage = async ({ key, isJson }) => {
+//   try {
+//     const value = await AsyncStorage.getItem(key);
 
-    if (value !== null) {
-      return isJson ? JSON.parse(value) : value;
-    }
-    return null;
-  } catch (e) {
-    console.log('getStorageError: ', e);
-  }
-};
+//     if (value !== null) {
+//       return isJson ? JSON.parse(value) : value;
+//     }
+//     return null;
+//   } catch (e) {
+//     console.log('getStorageError: ', e);
+//   }
+// };
