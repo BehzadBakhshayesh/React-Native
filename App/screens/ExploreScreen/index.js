26ac177cc9679022da4ec7fb2691d8f759ccae44
bhzd <bhzd.bakhshayesh@gmail.com>
2023-07-14 18:55:31 +0330

    code

--- App/screens/ExploreScreen/index.js
+++ App/screens/ExploreScreen/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { SafeAreaView, ScrollView, TextInput, StatusBar, StyleSheet } from 'react-native';
 import propTypes from 'prop-types';
+import ExploreModel from '@app/components/ExploreModel';
+
+const data = [1, 2, 3, 4, 5, 6, 7];
 
 function ExploreScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Explore Screen</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <TextInput placeholder="Search" style={styles.input} />
+      <ScrollView>
+        {data?.map((item) => (
+          <Fragment key={item}>
+            <ExploreModel />
+          </Fragment>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 ExploreScreen.propTypes = {
   navigation: propTypes.object,
 };
 export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  wrapper: { flex: 1, paddingTop: StatusBar?.currentHeight },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
